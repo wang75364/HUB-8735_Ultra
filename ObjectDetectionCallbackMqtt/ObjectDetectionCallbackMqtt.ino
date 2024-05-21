7040bff47a0f903c5af968f0aba55c642ee80f4c
wang75364 <135223462+wang75364@users.noreply.github.com>
2024-05-22 03:41:15 +0800

    Add files via upload

---
+++ ObjectDetectionCallbackMqtt/ObjectDetectionCallbackMqtt.ino
@@ -0,0 +1,222 @@
+/*
+
+ Example guide:
+ https://www.amebaiot.com/en/amebapro2-arduino-neuralnework-object-detection/
+
+ NN Model Selection
+ Select Neural Network(NN) task and models using .modelSelect(nntask, objdetmodel, facedetmodel, facerecogmodel).
+ Replace with NA_MODEL if they are not necessary for your selected NN Task.
+
+ NN task
+ =======
+ OBJECT_DETECTION/ FACE_DETECTION/ FACE_RECOGNITION
+
+ Models
+ =======
+ YOLOv3 model         DEFAULT_YOLOV3TINY   / CUSTOMIZED_YOLOV3TINY
+ YOLOv4 model         DEFAULT_YOLOV4TINY   / CUSTOMIZED_YOLOV4TINY
+ YOLOv7 model         DEFAULT_YOLOV7TINY   / CUSTOMIZED_YOLOV7TINY
+ SCRFD model          DEFAULT_SCRFD        / CUSTOMIZED_SCRFD
+ MobileFaceNet model  DEFAULT_MOBILEFACENET/ CUSTOMIZED_MOBILEFACENET
+ No model             NA_MODEL
+ */
+
+#include "WiFi.h"
+#include "StreamIO.h"
+#include "VideoStream.h"
+#include "RTSP.h"
+#include "NNObjectDetection.h"
+#include "VideoStreamOverlay.h"
+#include "ObjectClassList.h"
+#include <PubSubClient.h>
+
+#define CHANNEL   0
+#define CHANNELNN 3
+
+// Lower resolution for NN processing
+#define NNWIDTH  640
+#define NNHEIGHT 640
+
+VideoSetting config(VIDEO_FHD, 10, VIDEO_H264, 0);
+VideoSetting configNN(NNWIDTH, NNHEIGHT, 10, VIDEO_RGB, 0);
+NNObjectDetection ObjDet;
+RTSP rtsp;
+StreamIO videoStreamer(1, 1);
+StreamIO videoStreamerNN(1, 1);
+
+char ssid[] = "5487";    // your network SSID (name)
+char pass[] = "19198181";        // your network password
+int status = WL_IDLE_STATUS;
+
+IPAddress ip;
+int rtsp_portnum;
+char mqttServer[] = "broker.emqx.io";
+char clientId[] = "HUB8735Client";
+char publishTopic[] = "8735topic1";
+char publishPayload[] = "hello world";
+char subscribeTopic[] = "8735topic2";
+
+void callback(char* topic, byte* payload, unsigned int length);
+
+WiFiClient wifiClient;
+PubSubClient client(mqttServer, 1883, callback, wifiClient);
+
+// Callback function
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  Serial.println();
+
+  // In order to republish this payload, a copy must be made
+  // as the original payload buffer will be overwritten whilst
+  // constructing the PUBLISH packet.
+
+  // Allocate the correct amount of memory for the payload copy
+  byte* p = (byte*)(malloc(length));
+  // Copy the payload to the new buffer
+  memcpy(p, payload, length);
+  client.publish(publishTopic, p, length);
+  // Free the memory
+  free(p);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!(client.connected())) {
+    Serial.print("\r\nAttempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientId)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement and resubscribe
+      client.publish(publishTopic, publishPayload);
+      client.subscribe(subscribeTopic);
+    } else {
+      Serial.println("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void setup()
+{
+    Serial.begin(115200);
+
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to WPA SSID: ");
+        Serial.println(ssid);
+        status = WiFi.begin(ssid, pass);
+
+        // wait 2 seconds for connection:
+        delay(2000);
+    }
+    ip = WiFi.localIP();
+
+    // Configure camera video channels with video format information
+    // Adjust the bitrate based on your WiFi network quality
+    config.setBitrate(2 * 1024 * 1024);    // Recommend to use 2Mbps for RTSP streaming to prevent network congestion
+    Camera.configVideoChannel(CHANNEL, config);
+    Camera.configVideoChannel(CHANNELNN, configNN);
+    Camera.videoInit();
+
+    // Configure RTSP with corresponding video format information
+    rtsp.configVideo(config);
+    rtsp.begin();
+    rtsp_portnum = rtsp.getPort();
+
+    // Configure object detection with corresponding video format information
+    // Select Neural Network(NN) task and models
+    ObjDet.configVideo(configNN);
+    ObjDet.setResultCallback(ODPostProcess);
+    ObjDet.modelSelect(OBJECT_DETECTION, CUSTOMIZED_YOLOV7TINY, NA_MODEL, NA_MODEL);
+    ObjDet.begin();
+
+    // Configure StreamIO object to stream data from video channel to RTSP
+    videoStreamer.registerInput(Camera.getStream(CHANNEL));
+    videoStreamer.registerOutput(rtsp);
+    if (videoStreamer.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+
+    // Start data stream from video channel
+    Camera.channelBegin(CHANNEL);
+
+    // Configure StreamIO object to stream data from RGB video channel to object detection
+    videoStreamerNN.registerInput(Camera.getStream(CHANNELNN));
+    videoStreamerNN.setStackSize();
+    videoStreamerNN.setTaskPriority();
+    videoStreamerNN.registerOutput(ObjDet);
+    if (videoStreamerNN.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+
+    // Start video channel for NN
+    Camera.channelBegin(CHANNELNN);
+
+    // Start OSD drawing on RTSP video channel
+    OSD.configVideo(CHANNEL, config);
+    OSD.begin();
+
+    if (client.connect(clientId)) {
+    client.publish(publishTopic, publishPayload);
+    client.subscribe(subscribeTopic);
+  }
+}
+
+void loop()
+{
+    if (!(client.connected())) {
+    reconnect();
+    }
+    client.loop();
+}
+
+// User callback function for post processing of object detection results
+void ODPostProcess(std::vector<ObjectDetectionResult> results)
+{
+    uint16_t im_h = config.height();
+    uint16_t im_w = config.width();
+
+    Serial.print("Network URL for RTSP Streaming: ");
+    Serial.print("rtsp://");
+    Serial.print(ip);
+    Serial.print(":");
+    Serial.println(rtsp_portnum);
+    Serial.println(" ");
+
+    printf("Total number of objects detected = %d\r\n", ObjDet.getResultCount());
+    OSD.createBitmap(CHANNEL);
+
+    if (ObjDet.getResultCount() > 0) {
+        for (int i = 0; i < ObjDet.getResultCount(); i++) {
+            int obj_type = results[i].type();
+            if (itemList[obj_type].filter) {    // check if item should be ignored
+
+                ObjectDetectionResult item = results[i];
+                // Result coordinates are floats ranging from 0.00 to 1.00
+                // Multiply with RTSP resolution to get coordinates in pixels
+                int xmin = (int)(item.xMin() * im_w);
+                int xmax = (int)(item.xMax() * im_w);
+                int ymin = (int)(item.yMin() * im_h);
+                int ymax = (int)(item.yMax() * im_h);
+
+                // Draw boundary box
+                printf("Item %d %s:\t%d %d %d %d\n\r", i, itemList[obj_type].objectName, xmin, xmax, ymin, ymax);
+                OSD.drawRect(CHANNEL, xmin, ymin, xmax, ymax, 3, OSD_COLOR_WHITE);
+
+                // Print identification text
+                char text_str[20];
+                snprintf(text_str, sizeof(text_str), "%s %d", itemList[obj_type].objectName, item.score());
+                OSD.drawText(CHANNEL, xmin, ymin - OSD.getTextHeight(CHANNEL), text_str, OSD_COLOR_CYAN);
+
+                char text_str2[128];
+                snprintf(text_str2, sizeof(text_str2), "Item %d %s:\t%d %d %d %d", i, itemList[obj_type].objectName, xmin, xmax, ymin, ymax);
+                client.publish(publishTopic, text_str2);
+            }
+        }
+    }
+    OSD.update(CHANNEL);
+}
